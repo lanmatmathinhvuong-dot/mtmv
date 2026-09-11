e48eb30ac26e0125865345a55bb403fcb10d2c1a
Ngo Thi Lan <lanmatmathinhvuong@gmail.com>
2026-09-11 23:29:34 +0700

    xu ly canh bao hydration admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+   <html
+  lang="vi"
+  suppressHydrationWarning
+  className={`${geistSans.variable} ${geistMono.variable}`}
+>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
